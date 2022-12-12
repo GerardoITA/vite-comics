ad98a7d4c7b5b1b597153d19553a171293ee6491
GerardoITA <gerardo.giusti@tiscali.it>
2022-12-12 22:41:57 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import HeaderDC from './components/HeaderDC.vue'
+
+export default {
+  name: "App",
+  components: {
+    HeaderDC
+  }
+}
 </script>
 
 <template>
-<!--   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" /> -->
+ <HeaderDC></HeaderDC>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+@use "./styles/partials/variables" as *;
+@use "./styles/partials/mixins" as *;
+@use "./styles/general.scss" as *;
 </style>
---
+++ src/components/HeaderDC.vue
@@ -0,0 +1,107 @@
+<script>
+    export default {
+        name: "HeaderDC",
+        data(){
+            return {
+                links: [
+                {   url: "#",
+                    text: "CHARACTERS",
+                    current: false,
+                },
+                {   url: "#",
+                    text: "COMICS",
+                    current: true,
+                },
+                {   url: "#",
+                    text: "MOVIES",
+                    current: false,
+                },
+                {   url: "#",
+                    text: "TV",
+                    current: false,
+                },
+                {   url: "#",
+                    text: "GAMES",
+                    current: false,
+                },
+                {   url: "#",
+                    text: "COLLECTIBLES",
+                    current: false,
+                },
+                {   url: "#",
+                    text: "VIDEOS",
+                    current: false,
+                },
+                {   url: "#",
+                    text: "FANS",
+                    current: false,
+                },
+                {   url: "#",
+                    text: "NEWS",
+                    current: false,
+                },
+                {   url: "#",
+                    text: "SHOP",
+                    current: false,
+                },
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="outer">
+        <a href="/" class="logoDC">
+            <img src="../assets/dc-logo.png" alt="Logo DC">
+        </a>
+
+        <nav>
+            <ul>
+                <li v-for="(link, index) in links" :key="index">
+                    <a :href="link.url" :class="{active: link.current}">
+                        {{link.text}}
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+    .outer {
+        @include center(both);
+        gap: 20%;
+        height: 10rem;
+    }
+    nav {
+        box-sizing: border-box;
+        ul {
+                display: flex;
+        
+                li {
+                    list-style: none;
+                    margin: 0 2rem 0;
+        
+                    a {
+                        display: inline-block;
+                        padding: 3rem 0;
+                        text-decoration: none;
+                        color: $DCgrey;
+                        font-weight: 600;
+                       
+        
+                        &:active,
+                        &:hover {
+                            color: $DCblue;
+                            border-bottom: 6px solid $DCblue;
+                        }
+                    }
+        
+                }
+            }
+    }
+</style>
